3d16d9dda4f4c60bfc8c2b06dcf9a5d4b1e564b4
rml60 <r.m-l@gmx.de>
2020-12-29 10:01:13 +0100

    "THE BEER-WARE LICENSE" hinzugefügt.

--- mcan/mcancommand.py
+++ mcan/mcancommand.py
@@ -1,6 +1,13 @@
 """ mcancommand.py
 
-    Autor: Maier-Lohman, Rainer
+    Author: Rainer Maier-Lohmann
+    ---------------------------------------------------------------------------
+    "THE BEER-WARE LICENSE" (Revision 42):
+    <r.m-l@gmx.de> wrote this file.  As long as you retain this notice you
+    can do whatever you want with this stuff. If we meet some day, and you 
+    think this stuff is worth it, you can buy me a beer in return.
+    ---------------------------------------------------------------------------
+    (c) 2020
 """
 from .mcanmsgarray import McanMsgArray
 
--- mcan/mcandecode.py
+++ mcan/mcandecode.py
@@ -2,7 +2,14 @@
     Modul zur Verwaltung, Analyse und Ersstellung
     von Maerklin CAN-Bus Meldungen
 
-    Autor: Maier-Lohmann, Rainer
+    Author: Rainer Maier-Lohmann
+    ---------------------------------------------------------------------------
+    "THE BEER-WARE LICENSE" (Revision 42):
+    <r.m-l@gmx.de> wrote this file.  As long as you retain this notice you
+    can do whatever you want with this stuff. If we meet some day, and you 
+    think this stuff is worth it, you can buy me a beer in return.
+    ---------------------------------------------------------------------------
+    (c) 2020
 """
 from struct import unpack
 from os import linesep
